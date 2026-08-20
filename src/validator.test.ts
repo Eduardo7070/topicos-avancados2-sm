e60e0a9609f637b715efb5829042e6f60628ef02
EDUARDO DA SILVA NASCIMENTO <eduardo.nascimento2006@cs.cruzeirodosul.edu.br>
2026-08-20 19:49:13 -0300

    adiciona validação e testes automatizados para minimo e maximo de caracters

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -20,4 +20,18 @@ describe('Suíte de Testes - Validador de Chamados (Service Desk)', () => {
             validarChamado("   ");
         }).toThrow("O título do chamado não pode ser vazio.");
     });
+
+     test('Deve lançar erro se o título for menor que 5 caracters', () => {
+        expect(() => {
+            validarChamado("adsa");
+        }).toThrow("O título deve possuir entre 5 e 100 caracteres.");
+    });
+
+    test('Deve lançar erro se o título tiver mais que 100 caracteres', () => {
+        const titulo101Caracteres = "a".repeat(101);
+        
+        expect(() => {
+            validarChamado(titulo101Caracteres);
+        }).toThrow("O título deve possuir entre 5 e 100 caracteres.");
+    });
 });
--- src/validator.ts
+++ src/validator.ts
@@ -3,6 +3,10 @@ function validarChamado(titulo: string): boolean {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
+    if (titulo.length < 5 || titulo.length > 100) {
+        throw new Error("O título deve possuir entre 5 e 100 caracteres.");
+    }
+
     return true;
 }
 
